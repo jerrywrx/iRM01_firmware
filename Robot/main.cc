a4d11f5284473b4b906cc27bbd3084d7addfbe00
jerrywrx <jerrywrx@foxmail.com>
2023-11-25 23:43:11 -0600

    4310 not working

--- Robot/main.cc
+++ Robot/main.cc
@@ -11,13 +11,15 @@
 
 #define TARGET_SPEED 30
 
-static BoolEdgeDetector init(false);
+static BoolEdgeDetector button_init(false);
 
 bsp::CAN* can = nullptr;
-control::MotorCANBase* motor = nullptr;
+//control::MotorCANBase* motor = nullptr;
 control::Motor4310* leg_motor = nullptr;
 remote::DBUS* dbus;
 BMI088 imu;
+
+static bool init = false;
 
 /* callback triggered by an external interrupt on a GPIO pin*/
 void HAL_GPIO_EXTI_Callback(uint16_t GPIO_Pin) {
@@ -50,14 +52,13 @@ void RTOS_Init() {
 	BMI088_Init(&imu, &hspi1, GPIOB, SPI1_CS_ACC_Pin, GPIOA, SPI1_CS_GYRO_Pin);
 
 	can = new bsp::CAN(&hcan, true);
-	motor = new control::Motor3508(can, 0x201);
+//	motor = new control::Motor3508(can, 0x201);
     leg_motor = new control::Motor4310(can, 0x02, 0x01, control::MIT);
     dbus = new remote::DBUS(&huart2);
 }
 
 
 uint16_t zeroDriftTry = 1000;
-bool calibrated = false;
 
 void imuTask(const void* args){
 	UNUSED(args);
@@ -86,6 +87,9 @@ void imuTask(const void* args){
 //		}
 //	}
 
+    while (dbus->swr != remote::DOWN) {HAL_Delay(10);}
+    init = true;
+
 	while (true) {
 //        if (calibrated) {
 //            if (++count < zeroDriftTry) {
@@ -113,31 +117,34 @@ void imuTask(const void* args){
 	}
 }
 
-bool motor_start = false;
+bool button_pressed = false;
 void chassisTask(const void* args){
 	UNUSED(args);
 
     while (true) {
-        init.input(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13));
-        if (init.negEdge()) {
-            motor_start = true;
+        button_init.input(HAL_GPIO_ReadPin(GPIOB, GPIO_PIN_13));
+        if (button_init.negEdge()) {
+            button_pressed = true;
             break;
         }
     }
 
-    control::MotorCANBase* motors[] = {motor};
-    control::PIDController pid(20, 15, 30);
+    while (dbus->swr != remote::DOWN) {HAL_Delay(10);}
+
+//    control::MotorCANBase* motors[] = {motor};
+//    control::PIDController pid(20, 15, 30);
     control::Motor4310* leg_motors[] = {leg_motor};
 
     leg_motor->SetZeroPos();
     leg_motor->MotorEnable();
 
 	while (true) {
-        float diff = motor->GetOmegaDelta(TARGET_SPEED);
-        int16_t out = pid.ComputeConstrainedOutput(diff);
-        motor->SetOutput(out);
-        control::MotorCANBase::TransmitOutput(motors, 1);
+//        float diff = motor->GetOmegaDelta(TARGET_SPEED);
+//        int16_t out = pid.ComputeConstrainedOutput(diff);
+//        motor->SetOutput(out);
+//        control::MotorCANBase::TransmitOutput(motors, 1);
 
+//        uint16_t vel = dbus->ch1 / 660 * 30;
         leg_motor->SetOutput(0, 1, 0, 0.5, 0);
         control::Motor4310::TransmitOutput(leg_motors, 1);
 
@@ -174,8 +181,6 @@ void ledTask(const void* args){
         	HAL_GPIO_WritePin(GPIOB, GPIO_PIN_0, GPIO_PIN_RESET);
         }
 
-//        print("Intensity: %d   Pin state: %d\r\n", intensity, pin_state);
-
 		HAL_Delay(50);
 	}
 
@@ -186,17 +191,25 @@ void RTOS_Default_Task(const void* args) {
 	UNUSED(args);
 
     while (true) {
+        set_cursor(0, 0);
+        clear_screen();
+        print("# %.2f s, IMU %s\r\n", HAL_GetTick() / 1000.0,
+              init ? "\033[1;42mReady\033[0m" : "\033[1;41mNot Ready\033[0m");
+
+        print("button_pressed: %d\r\n", button_pressed);
+
+//        print("Euler Angles: %.2f, %.2f, %.2f\r\n", imu.INS_euler[0] / PI * 180,
+//              imu.INS_euler[1] / PI * 180, imu.INS_euler[2] / PI * 180);
+
 //        print("acc0: %.3f, acc1: %.3f, acc2: %.3f, gyr0: %.3f, gyr1: %.3f, gyr2: %.3f\r\n",
 //            imu.acc_mps2[0], imu.acc_mps2[1], imu.acc_mps2[2], imu.gyr_rps[0], imu.gyr_rps[1], imu.gyr_rps[2]);
-
-    	clear_screen();
-        set_cursor(0, 0);
-
-        print("CH0: %-4d CH1: %-4d CH2: %-4d CH3: %-4d ", dbus->ch0, dbus->ch1, dbus->ch2, dbus->ch3);
+//
+//        print("CH0: %-4d CH1: %-4d CH2: %-4d CH3: %-4d ", dbus->ch0, dbus->ch1, dbus->ch2, dbus->ch3);
 //        print("SWL: %d SWR: %d DIAL: %d @ %d ms\r\n", dbus->swl, dbus->swr, dbus->wheel, dbus->timestamp);
-
+//
 //    	print("motor_start: %d\r\n", motor_start);
 //        print("yaw: %.3f, pitch: %.3f, roll: %.3f\r\n", imu.INS_euler[0] / PI * 180, imu.INS_euler[1] / PI * 180, imu.INS_euler[2] / PI * 180);
+
         HAL_Delay(10);
     }
 
